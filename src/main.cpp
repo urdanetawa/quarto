1d5d0c75185fd394c6b1f3fddb2d67f6387dd8ea
Wilson Urdaneta <wilson.urdaneta@gmail.com>
2011-05-10 10:19:32 +0800

    First commit. Does not compile at all, this is just the skeleton that we might use in the development

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+// main.cpp
+//
+// 10 May 2011
+//
+// Developed by
+// Wilfred Wong <>
+// Wilson A. Urdaneta <20684149>
+//
+
+#include "player.h"
+#include "quarto.h"
+#include "playeGame.h"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Human player
+  Player *humanPlayer;
+  // Computer player
+  //Player *computerPlayer;
+  
+  // Quarto game
+  //Quarto *quarto;
+  
+  // 
+  //PlayGame *playGame = new PlayGame( quarto, humanPlayer, computerPlayer );
+  
+  // Setting player 1
+  //playGame->setPlayer1( humanPlayer );
+  // Setting player 2
+  //playGame->setPlayer2( computerPlayer );
+    
+  //while ( quarto->getGameStatus() != playGame::end )
+  //{
+	//playGame->takeTurn();
+  //}
+  
+  //delete humanPlayer;
+  //delete computerPlayer;
+  //delete quarto;
+  //delete playGame;
+  
+  return 0;
+}
